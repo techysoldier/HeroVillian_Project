4a2b6b1e7a21c043e82c69e3db4f82f9c8ce6bcb
Devin-Symone Johnson <devinsymone@gmail.com>
2022-04-04 17:45:05 -0400

    revert previous edition

--- supers/views.py
+++ supers/views.py
@@ -1,8 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Super
+from .serializers import SuperSerializer
 
 @api_view(['GET'])
 def super_list(request): 
-
+        suppers = Super.objects.all()
+        serializer = SuperSerializer
 
         return Response ('ok')
