842946b0754ca12c664486c10843cafe3e922773
Devin-Symone Johnson <devinsymone@gmail.com>
2022-04-05 12:15:40 -0400

    all postman data added, parameters added, +error: does not pull villians down, remains a list [] in postman check

--- supers/urls.py
+++ supers/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
+from django.urls import path 
 from . import views
 
-
 urlpatterns = [
-    path('', views.super_list),
-     path('<int:pk>/', views.super_detail),
+    path('', views.supers_list),
+    path('<int:pk>/', views.super_detail),
 ]
--- supers/views.py
+++ supers/views.py
@@ -1,40 +1,53 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Super
-from .serializers import SuperSerializer
 from rest_framework import status
 
-from supers import serializers 
+from super_types.views import Super_type
+from .serializers import SuperSerializer
+from .models import Super
 
 
 
 @api_view(['GET','POST'])
-def super_list(request):
+def supers_list(request):
     if request.method == 'GET':
+        custom_response_dictionary = {}
+        type_param = request.query_params.get('type')
         supers = Super.objects.all()
-        serializer = SuperSerializer(supers, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        if type_param:
+            supers = supers.filter(super_type__type = type_param)
+            serializer = SuperSerializer(supers,many = True)
+            return Response(serializer.data)
+        else:
+            super_types = Super_type.objects.all()
+            for type in super_types:
+                supers = Super.objects.filter(super_type_id = type.id)
+                serializer = SuperSerializer(supers, many = True)
+                custom_response_dictionary[type.type] = serializer.data
+            return Response(custom_response_dictionary)
+
     elif request.method == 'POST':
-        serializer = SuperSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializer = SuperSerializer(data = request.data)
+        if serializer.is_valid(raise_exception = True):
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
 
 
-
-    
-@api_view(['GET','PUT', 'DELETE'])
-def super_detail(request, pk):
-   super =get_object_or_404(pk=pk)
-   if request.method == 'GET':     
+@api_view(['GET','PUT','DELETE'])
+def super_detail(request,pk):
+    super = get_object_or_404(Super, pk = pk)
+    if request.method == 'GET':
         serializer = SuperSerializer(super)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-   elif request.method == 'PUT':
-        serializer = SuperSerializer(super, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-   elif request.method =='DELETE':
+        return Response(serializer.data, status.HTTP_200_OK)
+    if request.method == 'PUT':
+        serializer = SuperSerializer(super,data = request.data)
+        if serializer.is_valid(raise_exception = True):
+            serializer.save()
+            return Response(serializer.data, status.HTTP_200_OK)
+    if request.method == 'DELETE':
         super.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)         
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+   
